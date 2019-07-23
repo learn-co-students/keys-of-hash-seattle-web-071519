e65877b8036e423f7374668529b7a1de3ec2f5fb
ayerest <irisiseve@gmail.com>
2019-07-23 11:44:03 -0700

    finished keys of hash lab

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    self.select do |key, value|
+      arguments.include?(value)
+    end.keys
   end
 end
